01ed73233b999e40167e070b23364318c3425ed1
ilovepepa23 <ilovepepa23@gmail.com>
2022-05-15 23:22:28 -0700

    init

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-document.querySelectorAll('.accordion__butto').forEach(button => {
-  button.addEventListener('click', {} => {
-    const accordionContent = button.nextElementSibling;
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-    button.classList.toggle('accorion__button--active');
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
 
-    if (button.classList.contains('acordion__button active')) {
-      accordionConstant.style.maxHeight - accordionContent.scrollHeight + 'px';
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
   });
-});
+}
